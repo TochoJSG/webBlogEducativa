164df6004736b191f13a6ebcf7d049b0095191b6
TochoJSG(JogeSalgado) <jjorgess081@gmail.com>
2025-02-07 21:53:49 -0600

    version beta

--- src/componentes/Proximamente.jsx
+++ src/componentes/Proximamente.jsx
@@ -1,15 +1,20 @@
 import HeaderGral from "./HeaderGral";
 import Loader from "./Loader";
 import Footer from "./Footer";
+import Watsapp from "./Watsapp";
 
 const Proximamente = () =>{
     return(
         <>
             <HeaderGral />
+            
             <Loader 
                 title="Sitio en Desarrollo"
                 contenido="Comunicate al 55-91-08-82-27 Whatsapp o llamada para recibir asesoria en cualquier tramite vehicular dificultoso."
             />
+
+            <Watsapp />
+
             <Footer />
         </>
     );
